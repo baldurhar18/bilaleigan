42d2119eee93f10324864d1909b994555a8b2bf4
Birta Líf Baldursdóttir <birtalif@clients-pool-1.nat.ru.is>
2018-12-04 14:19:19 +0000

    sma test bara

--- birtatest.py
+++ birtatest.py
@@ -1,1 +1,9 @@
-#this is a git test
+print("Welcome to The Car Rental!")
+choice = input("1. for Costumers\n2. for Cars\nq for Quit\n")
+
+if choice == "1":
+    print("CARS")
+elif choice == "2":
+    print("COSTUMERS")
+elif choice == "q".upper():
+    print("quit")
