0a798cc4f709127f58fb7e54d17c28d53005ba54
Baldur Haraldsson <baldurhar18@ru.is>
2018-12-04 17:36:06 +0000

    Main menu

--- Lokaverkefni/Employee/EmployeeUI.py
+++ Lokaverkefni/Employee/EmployeeUI.py
@@ -1,13 +1,39 @@
 def front_Page():
-    chocie = ''
-    while chocie != 'q':
-        print('{0:40}{1}'.format('Bílaleigan', 'Front page'))
-        print('-'*50)
-        print("{0:>26}".format('Hello'))
-        print("Press - 1 for Cars \nPress - 2 for Customers \nPress q to Quit ")
-        print('-'*50)
-        print('{0:>18}{1:>18}'.format('Cars', 'Customers'))
-        chocie = input("")
+    print('{0:40}{1}'.format('Bílaleigan', 'Front page'))
+    print('-'*50)
+    print("{0:>26}".format('Hello'))
+    print('-'*50)
+    print("Press - 1 for Cars \nPress - 2 for Customers \nPress q to Quit ")
 
 
-front_Page()
+def car_Page():
+    print('{0:40}{1}'.format('Bílaleigan', 'B to back'))
+    print('-'*50)
+    print("{0:>26}".format('Cars'))
+    print('-'*50)
+    print("Press - 1 to Mark car as available for rent \nPress - 2 to Mark car as rented \nPress - 3 to Make Order \nPress - 4 to Cancel Order Press\nPress - 5 to Search for Order \nPress - 6 to Change Order\nPress - 7 for Cars Status\nPress - 8 for Price list\nPress q to Quit ")
+
+
+def customers_Page():
+    print('{0:40}{1}'.format('Bílaleigan', 'B to back'))
+    print('-'*50)
+    print("{0:>26}".format('Customers'))
+    print('-'*50)
+    print("Press - 1 for New Customer \nPress - 2 to Remove Customer  \nPress - 3 to Look up Customer\nPress - 4 to Edit Customer\nPress q to Quit ")
+
+
+def main():
+
+    choice = ''
+    front_Page()
+    while choice != 'q':
+        choice = input("")
+        if choice == "1":
+            car_Page()
+            choice = input("")
+            if
+        elif choice == "2":
+            customers_Page()
+
+
+main()
